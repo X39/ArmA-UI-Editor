1ce2f00639c44cdc3c43a604cbe4f3dba3f2c903
Marco.Silipo <m.silipo@web.de>
2016-05-20 21:51:58 +0200

    Added exception handler for crash.txt handler

--- ArmA UI Editor/App.xaml.cs
+++ ArmA UI Editor/App.xaml.cs
@@ -13,5 +13,27 @@ namespace ArmA_UI_Editor
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+#if DEBUG
+            System.Diagnostics.Debugger.Break();
+#endif
+            using (var writer = new System.IO.StreamWriter("crash.txt", false))
+            {
+                var ex = e.Exception;
+                int tabCount = 0;
+                while (ex != null)
+                {
+                    writer.WriteLine(ex.Message);
+                    writer.WriteLine(ex.StackTrace.Replace("\r", new string('\t', tabCount) + '\n'));
+                    ex = ex.InnerException;
+                    tabCount++;
+                }
+                writer.Flush();
+            }
+            MessageBox.Show("An unhandled exception was found ...\nPlease report the crash.txt at https://github.com/X39/ArmA-UI-Editor/issues/new\nThe issue will be fixed ASAP :)\n\nSorry for your lost work (in case you did not saved) ...", "Fatal Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            App.Current.Shutdown();
+            e.Handled = true;
+        }
     }
 }
