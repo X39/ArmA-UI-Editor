af89f58f07f5b62f004b7711c9e637480be4f240
Marco.Silipo <m.silipo@web.de>
2016-06-18 01:36:53 +0200

    Further implemented AddInBrowser

--- ArmA UI Editor/UI/AddInBrowser.xaml.cs
+++ ArmA UI Editor/UI/AddInBrowser.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
 
 namespace ArmA_UI_Editor.UI
 {
@@ -19,9 +20,146 @@ namespace ArmA_UI_Editor.UI
     /// </summary>
     public partial class AddInBrowser : Window
     {
+        [DllImport("shell32.dll")]
+        static extern int FindExecutable(string lpFile, string lpDirectory, [Out] StringBuilder lpResult);
+
+
         public AddInBrowser()
         {
             InitializeComponent();
+            this.lb_AddIns.SelectedValuePath = "";
+            this.lb_AddIns.DisplayMemberPath = "Info.Name";
+            foreach (var it in Code.AddInManager.Instance.AddIns)
+            {
+                this.lb_AddIns.Items.Add(it);
+            }
+            if(this.lb_AddIns.Items.Count > 0)
+            {
+                for (int i = 0; i < this.lb_AddIns.Items.Count; i++)
+                {
+                    var addin = this.lb_AddIns.Items[i] as Code.AddIn;
+                    if(addin.Info.Name.Equals("base", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        this.lb_AddIns.SelectedIndex = i;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void lb_AddIns_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.LoadAddin(((sender as ListBox).SelectedValue as Code.AddIn));
+        }
+        private void LoadAddin(Code.AddIn addin)
+        {
+            this.sp_AddInContent.Children.Clear();
+            UI.PropertyGroup pg;
+
+            pg = new PropertyGroup();
+            pg.Header.Text = "Author info";
+            this.sp_AddInContent.Children.Add(pg);
+            {
+                Property p;
+                TextBlock tb;
+
+                p = new Property();
+                pg.ItemsPanel.Children.Add(p);
+                p.Title.Text = "Author";
+                tb = new TextBlock(new Run(addin.Info.Author));
+                tb.Foreground = App.Current.Resources["SCB_DefaultTextColor"] as SolidColorBrush;
+                p.ConfigElement.Content = tb;
+
+                p = new Property();
+                pg.ItemsPanel.Children.Add(p);
+                p.Title.Text = "Description";
+                tb = new TextBlock(new Run(addin.Info.Description));
+                tb.Foreground = App.Current.Resources["SCB_DefaultTextColor"] as SolidColorBrush;
+                p.ConfigElement.Content = tb;
+
+                p = new Property();
+                pg.ItemsPanel.Children.Add(p);
+                p.Title.Text = "Version";
+                tb = new TextBlock(new Run(addin.Info.Version));
+                tb.Foreground = App.Current.Resources["SCB_DefaultTextColor"] as SolidColorBrush;
+                p.ConfigElement.Content = tb;
+            }
+
+            pg = new PropertyGroup();
+            pg.Header.Text = "UI-Elements";
+            this.sp_AddInContent.Children.Add(pg);
+            foreach (var it in addin.UIElements)
+            {
+                StackPanel panel = new StackPanel();
+                panel.Orientation = Orientation.Horizontal;
+                Image img = new Image();
+                img.Source = new BitmapImage(new Uri(it.Image));
+                img.Margin = new Thickness(0, 0, 2, 0);
+                panel.Children.Add(img);
+                TextBlock tb = new TextBlock();
+                tb.Text = it.DisplayName;
+                tb.Foreground = App.Current.Resources["SCB_DefaultTextColor"] as SolidColorBrush;
+                panel.MouseEnter += (sender, e) =>
+                {
+                    (sender as StackPanel).Background = App.Current.Resources["SCB_UIBackgroundLight"] as SolidColorBrush;
+                };
+                panel.MouseLeave += (sender, e) =>
+                {
+                    (sender as StackPanel).Background = Brushes.Transparent;
+                };
+                panel.MouseLeftButtonDown += (sender, e) =>
+                {
+                    if (e.ClickCount == 2)
+                    {
+                        var builder = new StringBuilder();
+                        int res = FindExecutable(it.__ClassPath, null, builder);
+                        switch (res)
+                        {
+                            case 31:
+                                System.Diagnostics.Process.Start("notepad.exe", it.__ClassPath);
+                                break;
+                            case 2:
+                            case 3:
+                            case 5:
+                            case 8:
+                                MessageBox.Show("Unable to open " + it.__ClassPath + "\r\n" + res, "whoops", MessageBoxButton.OK, MessageBoxImage.Warning);
+                                break;
+                            default:
+                                System.Diagnostics.Process.Start(builder.ToString(), it.__ClassPath);
+                                break;
+                        }
+                    }
+                };
+                panel.Children.Add(tb);
+                pg.ItemsPanel.Children.Add(panel);
+            }
+
+            pg = new PropertyGroup();
+            pg.Header.Text = "Styles";
+            this.sp_AddInContent.Children.Add(pg);
+            foreach (var it in addin.Styles)
+            {
+                TextBlock tb = new TextBlock();
+                tb.Text = it.Name;
+                tb.Foreground = App.Current.Resources["SCB_DefaultTextColor"] as SolidColorBrush;
+                tb.MouseEnter += (sender, e) =>
+                {
+                    (sender as TextBlock).Background = App.Current.Resources["SCB_UIBackgroundLight"] as SolidColorBrush;
+                };
+                tb.MouseLeave += (sender, e) =>
+                {
+                    (sender as TextBlock).Background = Brushes.Transparent;
+                };
+                tb.MouseLeftButtonDown += (sender, e) =>
+                {
+                    if (e.ClickCount == 2)
+                    {
+                        MessageBox.Show("Style switch not yet implemented :(", "whoops", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                };
+                pg.ItemsPanel.Children.Add(tb);
+            }
+
         }
     }
 }
