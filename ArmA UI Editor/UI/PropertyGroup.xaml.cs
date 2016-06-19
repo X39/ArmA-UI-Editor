6cc3628602a5321df6f7e886b00248c2926b4071
Marco.Silipo <m.silipo@web.de>
2016-06-19 23:15:06 +0200

    Enabled PropertyGroup to be used in XAML

--- ArmA UI Editor/UI/PropertyGroup.xaml.cs
+++ ArmA UI Editor/UI/PropertyGroup.xaml.cs
@@ -12,14 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Markup;
 
 namespace ArmA_UI_Editor.UI
 {
     /// <summary>
     /// Interaction logic for PropertyGroup.xaml
     /// </summary>
+    [ContentProperty("Children")]
     public partial class PropertyGroup : UserControl
     {
+        public static readonly DependencyPropertyKey ChildrenProperty = DependencyProperty.RegisterReadOnly("Children", typeof(UIElementCollection), typeof(PropertyGroup), new PropertyMetadata());
+
+        public UIElementCollection Children { get { return (UIElementCollection)GetValue(ChildrenProperty.DependencyProperty); } private set { SetValue(ChildrenProperty, value); } }
+
         public static readonly DependencyProperty IsExpanedProperty = DependencyProperty.Register("IsExpaned", typeof(bool), typeof(PropertyGroup));
         public bool IsExpaned { get { return (bool)GetValue(IsExpanedProperty); } set { SetValue(IsExpanedProperty, value); } }
 
@@ -30,6 +36,7 @@ namespace ArmA_UI_Editor.UI
         {
             InitializeComponent();
             this.IsExpaned = true;
+            Children = ItemsPanel.Children;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
