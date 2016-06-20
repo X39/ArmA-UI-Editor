89b34110a2fefc076d86232e41436727e4734f4e
Marco.Silipo <m.silipo@web.de>
2016-06-20 16:45:10 +0200

    changed encapsulation

--- ArmA UI Editor/Code/Interface/ISnapWindow.cs
+++ ArmA UI Editor/Code/Interface/ISnapWindow.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace ArmA_UI_Editor.Code.Interface
 {
-    interface ISnapWindow
+    internal interface ISnapWindow
     {
         void UnloadSnap();
         void LoadSnap();
