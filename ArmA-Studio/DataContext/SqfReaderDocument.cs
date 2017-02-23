20ff8fa4c71b23dbdbba944988767a9ff5517b58
X39 <X39@users.noreply.github.com>
2017-02-23 02:45:21 +0100

    Added simple SQF linter highlighting ... some ... errors ... has to be improved

--- ArmA-Studio/DataContext/SqfReaderDocument.cs
+++ ArmA-Studio/DataContext/SqfReaderDocument.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Documents;
 using ICSharpCode.AvalonEdit.Highlighting;
 using ArmA.Studio.UI;
+using VirtualRealityEngine.SQF.Parser;
+using System.IO;
 
 namespace ArmA.Studio.DataContext
 {
@@ -29,7 +31,22 @@ namespace ArmA.Studio.DataContext
             this.BreakPointMargin = new BreakPointMargin(sf);
             this.Editor.TextArea.TextView.BackgroundRenderers.Add(this.BreakPointMargin);
             this.Editor.TextArea.LeftMargins.Insert(0, BreakPointMargin);
-            
+        }
+        protected override IEnumerable<SyntaxError> GetSyntaxErrors()
+        {
+            using (var memstream = new MemoryStream())
+            {
+                { //Load content into MemoryStream
+                    var writer = new StreamWriter(memstream);
+                    writer.Write(this.Document.Text);
+                    writer.Flush();
+                    memstream.Seek(0, SeekOrigin.Begin);
+                }
+                //Setup base requirements for the parser
+                var parser = new Parser(new Scanner(memstream));
+                parser.Parse();
+                return parser.errors.ErrorList.Select((it) => new SyntaxError() { StartOffset = it.Item1, Length = it.Item2, Message = it.Item3 });
+            }
         }
     }
 }
